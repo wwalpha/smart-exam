77c83907b23ffb751b415e81b979ec4fdb88983d
wwalpha <wwalpha@gmail.com>
2025-12-18 01:49:39 +0000

    update

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate"
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindcssAnimate],
+}
+
